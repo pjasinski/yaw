164fb557ce623cdea09c229f85978c3b4dde7e3c
Patrick Jasinski <pjasin1@students.towson.edu>
2019-04-02 19:49:11 -0400

    update server for port 3000

--- server.js
+++ server.js
@@ -1,28 +1,8 @@
-﻿require('rootpath')();
 const express = require('express');
 const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const jwt = require('_helpers/jwt');
-const errorHandler = require('_helpers/error-handler');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
-
-// use JWT auth to secure the api
-app.use(jwt());
-
-// api routes
-
-app.use('/users', require('./users/users.controller'));
-app.use('/forms', require('./forms/forms.controller'));
-
-// global error handler
-app.use(errorHandler);
-
+const https = require('http');
 // start server
-const port = process.env.NODE_ENV === 'production' ? 80 : 3000;
+const port = 3000;
 const server = app.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
