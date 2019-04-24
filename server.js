82101a18a11c26407570a8c410db81fa1b70d49b
Patrick Jasinski <pjasin1@students.towson.edu>
2019-04-24 12:16:03 -0400

    removed old server file

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const https = require('http');
-// start server
-const port = 3000;
-const server = app.listen(port, function () {
-    console.log('Server listening on port ' + port);
-});
