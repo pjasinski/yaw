e21b6b42e29533aeec7862af199b2c58efee93d4
Patrick Jasinski <pjasin1@students.towson.edu>
2019-04-28 14:09:46 -0400

    added variable icons based on heading

--- yaw/src/app/map/map.component.ts
+++ yaw/src/app/map/map.component.ts
@@ -15,11 +15,44 @@ export class MapComponent implements OnInit {
   homeLat: number = 39.3996;
   homeLon: number = -76.5239;
   public agmMap: AgmMap;
-  previousWindow: any;
+  iconUrl: any;
   
   constructor(private airService : AircraftService) {}
   displayFlightNumber(event) {
     console.log(event);
+  }
+
+  getIconURL(h: number) {
+    let direction = "nw"; // northwest by default
+    if (h >= 0 && h < 45) {
+      direction = "n";
+    }
+    if (h >= 45 && h < 90) {
+      direction = "ne";
+    }
+    if (h >=90 && h < 135) {
+      direction = "e";
+    }
+    if (h >=135 && h < 180) {
+      direction = "se";
+    }
+    if (h >=180 && h < 225) {
+      direction = "s";
+    }
+    if (h >= 225 && h < 270) {
+      direction = "sw";
+    }
+    if (h >= 270 && h < 315 ) {
+      direction = "w";
+    }
+    if (h >= 315 && h < 360) {
+      direction = "nw";
+    }
+   
+    let url = "https://patolento.com/planeicons/".concat(direction).concat(".png");
+    //console.log(url);
+    //console.log(h);
+    return url;
   }
   ngOnInit() {
     this.airService.returnAircraft().subscribe(data => {
