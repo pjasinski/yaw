e92d16a14ab17c427b8cf92da956e7af7f2291f2
Patrick Jasinski <patrick.jasinski6800@me.com>
2019-05-07 17:56:58 -0400

    added routes

--- yaw/src/app/app-routing.module.ts
+++ yaw/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MapComponent } from './map/map.component';
+import { AircraftListComponent } from './aircraft-list/aircraft-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  { path: '', component: MapComponent},
+  { path: 'list', component: AircraftListComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
