0a66fd27f06fb4193c371583200e958f20745ba9
Patrick Jasinski <patrick.jasinski6800@me.com>
2019-05-19 19:44:22 -0400

    final update

--- yaw-server/scripts/jsonscript.js
+++ yaw-server/scripts/jsonscript.js
@@ -2,28 +2,33 @@ fs = require('fs');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://yaw:yawdb@patolento.com:27017";
 
-var jsonfile = fs.readFileSync('/data/aircraft.json');
+var jsonfile = fs.readFileSync('/data/dump1090/aircraft.json');
 var jsonobj = JSON.parse(jsonfile);
-// take only aircraft with flight numbers and lat+lons and add the data to a new object 
+// take only aircraft with flight numbers and lat+lons and add the data to a new object
 // to write out to file
 var onlyFlightNumbers = [];
 jsonobj.aircraft.forEach(function(element) {
-    if (element.lat && element.lon && element.flight){
+
+    if ((element.lat && element.lon) || element.flight){
         element.timeSeen = Math.ceil((new Date()).getTime() / (1000*60)) * (1000*60); // round time to the nearest minute
+        if (element.flight){
+            element.flight = element.flight.trim();
+        }
         onlyFlightNumbers.push(element);
     }
-}); 
+});
 
 var dataToWrite = JSON.stringify(onlyFlightNumbers);
 console.log(onlyFlightNumbers);
-fs.writeFileSync('/srv/http/aircraft.json', dataToWrite);
+fs.writeFileSync('/data/aircraft/aircraft.json', dataToWrite);
 
 MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("aircraft");
     dbo.collection("aircraft").insertMany(onlyFlightNumbers, function(err, res) {
-	if (err) throw err;
-	console.log("Number of docs inserted: " + res.insertedCount);
-	db.close();
+        if (err) throw err;
+        console.log("Number of docs inserted: " + res.insertedCount);
+        db.close();
     });
 });
+
